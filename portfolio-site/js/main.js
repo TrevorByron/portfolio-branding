3fe8f1b95177402cad26268172aea8af82b53d7a
Trevor <trevorborden@gmail.com>
2017-01-10 09:44:17 -0600

    uptodate

--- portfolio-site/js/main.js
+++ portfolio-site/js/main.js
@@ -1,3 +1,5 @@
 window.sr = ScrollReveal();
-sr.reveal('.foo', {duration: 1700, reset: true});
+sr.reveal('.foo', {duration: 700, reset: true, scale: 1});
 sr.reveal('.bar', {duration: 3500});
+sr.reveal('.description', {duration: 1000, reset: true, scale: 1, origin: 'top',distance: '30px',});
+sr.reveal('.bucket', {duration: 700, reset: true, scale: .95, distance: '5px',});
