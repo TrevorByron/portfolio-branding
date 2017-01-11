ddfd2b16e3a65f824c890e417b39d2b5b4e293c0
Trevor <trevorborden@gmail.com>
2017-01-11 15:14:18 -0600

    reset True removed

--- portfolio-site/js/main.js
+++ portfolio-site/js/main.js
@@ -1,5 +1,5 @@
 window.sr = ScrollReveal();
-sr.reveal('.foo', {duration: 700, reset: true, scale: 1});
+sr.reveal('.foo', {duration: 700, scale: 1});
 sr.reveal('.bar', {duration: 3500});
-sr.reveal('.description', {duration: 1000, reset: true, scale: 1, origin: 'top',distance: '30px',});
-sr.reveal('.bucket', {duration: 700, reset: true, scale: .95, distance: '5px',});
+sr.reveal('.description', {duration: 1000, scale: 1, origin: 'top',distance: '30px',});
+sr.reveal('.bucket', {duration: 700,  scale: .95, distance: '5px',});
