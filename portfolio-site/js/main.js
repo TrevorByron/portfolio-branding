34662eb5228fa4cd33b37d10a6938463d730a1b4
Trevor <trevorborden@gmail.com>
2017-01-12 14:02:48 -0600

    about me added

--- portfolio-site/js/main.js
+++ portfolio-site/js/main.js
@@ -3,3 +3,7 @@ sr.reveal('.foo', {duration: 700, scale: 1});
 sr.reveal('.bar', {duration: 3500});
 sr.reveal('.description', {duration: 1000, scale: 1, origin: 'top',distance: '30px',});
 sr.reveal('.bucket', {duration: 700,  scale: .95, distance: '5px',});
+sr.reveal('.card1', {duration: 500, scale: 1});
+sr.reveal('.card2', {duration: 500, scale: 1, delay: 250});
+sr.reveal('.card3', {duration: 500, scale: 1, delay: 500});
+sr.reveal('.card4', {duration: 500, scale: 1, delay: 750});
