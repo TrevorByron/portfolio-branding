d2fb3f38364f299a9514df0d61a8b73e600195e8
Trevor <trevorborden@gmail.com>
2017-01-14 17:21:23 -0600

    map adjustments

--- portfolio-site/js/main.js
+++ portfolio-site/js/main.js
@@ -7,3 +7,4 @@ sr.reveal('.card1', {duration: 500, scale: 1});
 sr.reveal('.card2', {duration: 500, scale: 1, delay: 250});
 sr.reveal('.card3', {duration: 500, scale: 1, delay: 500});
 sr.reveal('.card4', {duration: 500, scale: 1, delay: 750});
+sr.reveal('.boo', {duration: 1000, scale: 1, origin: 'top',distance: '30px',});
